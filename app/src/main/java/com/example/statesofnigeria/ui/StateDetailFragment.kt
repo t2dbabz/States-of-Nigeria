183ef5e122e7470141f4c079e0a77e837c676c70
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-02-15 20:52:20 +0100

    Implement viewBinding for the fragments

--- app/src/main/java/com/example/statesofnigeria/ui/StateDetailFragment.kt
+++ app/src/main/java/com/example/statesofnigeria/ui/StateDetailFragment.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.statesofnigeria.R
+import com.example.statesofnigeria.databinding.FragmentStateDetailBinding
 
 
 class StateDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentStateDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_state_detail, container, false)
+        binding = FragmentStateDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/statesofnigeria/ui/StatesListFragment.kt
+++ app/src/main/java/com/example/statesofnigeria/ui/StatesListFragment.kt
@@ -6,16 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.statesofnigeria.R
+import com.example.statesofnigeria.databinding.FragmentStatesListBinding
 
 
 class StatesListFragment : Fragment() {
+
+    private lateinit var binding: FragmentStatesListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_states_list, container, false)
+        binding = FragmentStatesListBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
